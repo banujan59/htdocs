8279b2adcc22ea01a3801398e20d1fd563289b9f
banujan59 <banujan59@gmail.com>
2017-11-20 15:51:11 -0500

    Product page
    great work

---
+++ product.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Shoe++ - Home</title>
+		<meta charset="utf-8"/>
+		<link rel="icon" href="images/logo.png">
+		
+		<!-- Style sheets -->
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="css/style.css"/>
+		<link rel="stylesheet" href="css/product.css"/>
+		
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet"/>
+		<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+		
+		<!-- Scripts -->
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<script src="js/product.js"></script>
+	</head>
+	
+	<body>
+		<div class="topBar">
+			<div class="logo">Shoe++</div>
+			<div class="container">
+				<div class="row">
+					<form>
+						<input type="text" placeholder="Search Shoe++">
+					</form>
+				</div>
+			</div>
+		</div>
+			
+			<br/><br/><br/><br/><br/><br/><br/><br/>
+		<div class="container">
+			<div class="row">
+			<nav>
+				<ul>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="register.php">Register</a></li>
+					<li><a href="about.php">About Us</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+				</ul>
+			</nav>
+			</div>
+				<br/><br/><br/>
+			<div class="row">
+				<div class="col-md-3">
+					<header>
+						<h1>Product Name</h1>
+					</header>
+				</div>
+			</div> <!-- End row -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="photoBox"><img width="100%;" src="images/1.jpg"/></div>
+					<div id="backButton" class="photoNavButton">&lt;</div>
+					<div id="nextButton" class="photoNavButton">&gt;</div>
+				</div>
+			</div> <!-- End row -->
+			<div class="row">
+				<br/><br/><br/>
+				<div class="col-md-3">
+					<header>
+						<h1>Description</h1>
+					</header>
+				</div>
+			</div> <!-- End row -->
+			<div class="row">
+				<div class="col-md-12">
+					<p>
+						Enter Long ass description here.
+					</p>
+				</div>
+			</div> <!-- End row -->
+			
+
+			<hr/>
+
+			<footer>
+				<p>&copy; Shoe++ 2017</p>
+			</footer>
+
+		</div><!--/.container-->
+
+	</body>
+</html>
