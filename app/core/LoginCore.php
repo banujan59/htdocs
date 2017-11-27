20db44a639631fbb70707ae363e4fa51b54247b7
RubyStretch <yellowopera49@gmail.com>
2017-11-27 15:50:27 -0500

    If there is a 0 between the email and the hash, then the login is successful.

--- app/core/LoginCore.php
+++ app/core/LoginCore.php
@@ -1,23 +1,26 @@
 <?php
 class LoginCore{
-	public static function login($username, $password){
+	public static function login($email, $password){
 		$user = Controller::model('Users');
-		$users = $user->where('username','=',$username)->get();
-		
+		$users = $user->where('email','=',$email)->get();
+		echo $email;
 		if(isset($users[0])){
-			if(password_verify($password, $users[0]->password)){
-				$_SESSION['username'] = $username;
+			if(password_verify($password, $users[0]->PASSWORD_HASH)){
+				$_SESSION['email'] = $email;
 				$_SESSION['userID'] = $users[0]->ID;
+				$_SESSION['pwdt'] = $users[0]->PASSWORD_HASH;
+				echo $_SESSION['userID'];
 			}
+			echo $_SESSION['pwdt'];
 		}
 	}
 	
 	public static function isLoggedIn(){
-		return isset($_SESSION['username']);
+		return isset($_SESSION['uname']);
 	}
 	
 	public static function logout(){
-		unset($_SESSION['username']);
+		unset($_SESSION['uname']);
 		unset($_SESSION['userID']);
 	}
 }
