f40b6b32d31d85a69f21b8ad94e5b7cd97d189e4
RubyStretch <yellowopera49@gmail.com>
2017-11-27 14:47:05 -0500

    login1

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,4 +1,23 @@
 $(function()
 {
-	
+	$("#loginUserButton").click(function(e)
+	{
+		// prevent form from submitting
+		e.preventDefault();
+		
+		// get the form data
+		var email = $("input[name=email]").val();
+		var password = $("input[name=password]").val();
+		
+		// validate data object
+		var data = {
+			email : email,
+			password : password,
+		}
+		
+		$.post("/home/login", data, function(serverMessage)
+		{
+			if
+		});
+	});
 });
