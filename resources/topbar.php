856c01a348cb0df81d2dce240cf3062476f9ec85
banujan59 <banujan59@gmail.com>
2017-11-28 13:37:51 -0500

    Top bar changes

--- resources/topbar.php
+++ resources/topbar.php
@@ -4,9 +4,17 @@
 			<div class="container">
 				<div class="row">
 					<form>
-						<input type="text" placeholder="Search Shoe++">
+						<input id="searchBox" type="text" placeholder="Search Shoe++">
 					</form>
 				</div>
 			</div>
+			
+			<script>
+				$("#searchBox").keydown(function(e)
+				{
+					// prevent submission when enter is pressed
+					
+				});
+			</script>
 	<?php
 ?>
